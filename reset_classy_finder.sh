aea324e906c924c27f6312dafb65e4c35f814e98
Eric Ripa <eric@ripa.io>
2015-03-24 16:20:24 +0100

    changed so that the scripts doesn't depend on current working directory

--- reset_classy_finder.sh
+++ reset_classy_finder.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR=`dirname $0`
+
 # copy the originals back to the original directory
-sudo cp ./finder_backup.png /System/Library/CoreServices/Dock.app/Contents/Resources/finder.png 
-sudo cp ./finder@2x_backup.png /System/Library/CoreServices/Dock.app/Contents/Resources/finder@2x.png 
+sudo cp ${SCRIPT_DIR}/finder_backup.png /System/Library/CoreServices/Dock.app/Contents/Resources/finder.png
+sudo cp ${SCRIPT_DIR}/finder@2x_backup.png /System/Library/CoreServices/Dock.app/Contents/Resources/finder@2x.png
 
 # trash the iconcache, forcing the dock to rebuild it
 sudo rm -f `sudo find /private/var/folders -name com.apple.dock.iconcache`
